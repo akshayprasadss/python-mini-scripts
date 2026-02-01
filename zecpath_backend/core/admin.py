ab37fbeb3bf731ea3bf8b59634db8409e13c8700
akshayprasadss <akshayprasad014@gmail.com>
2026-02-01 22:49:24 +0530

    Django ORM models, admin setup, and backend API

--- zecpath_backend/core/admin.py
+++ zecpath_backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Job, Application
+
+admin.site.register(User)
+admin.site.register(Job)
+admin.site.register(Application)
 
 # Register your models here.
